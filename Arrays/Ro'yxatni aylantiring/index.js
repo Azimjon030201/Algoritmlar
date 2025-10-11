61fbca13c469d24faafa01bf9d04cb270d95a46d
Azimjon030201 <hazard030201@gmail.com>
2025-10-11 13:57:50 +0500

    Array 2-masala yechildi

--- Arrays/Ro'yxatni aylantiring/index.js
+++ Arrays/Ro'yxatni aylantiring/index.js
@@ -1,5 +1,12 @@
 function rotate(nums, k){
-    // pass
+    for (let i = 0; i < k; i++) {
+        let last = nums.pop()
+        nums.unshift(last)
+        
+    }
+    return nums;
+
 }
+console.log(rotate([1,2,3,4,5,6,7],3));
 
 module.exports = {rotate}
