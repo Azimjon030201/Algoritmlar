f4641c958ebc912e4d0661bf21b7bcd014b69183
Azimjon030201 <hazard030201@gmail.com>
2025-10-13 16:45:02 +0500

    Paskal uchburchagi yechildi

--- Arrays/Paskal uchburchagi/index.js
+++ Arrays/Paskal uchburchagi/index.js
@@ -1,10 +1,26 @@
-function generate(n){
-    let result = [[1]]
-    let startingArr = [1,1]
-    for(let i = 1; i<n; i++){
-        
+function generateRow(prev){
+    let next = [1]
+    for (let i = 0; i < prev.length - 1; i++) {
+        next.push(prev[i] + prev[i+1])
+
         
     }
+    next.push(1)
+    return next
+}
+function generate(n){
+    if(n == 0){
+        return []
+    }
+    let row = [1]
+    let result = [row]
+    for (let i = 0; i < n; i++) {
+        row  = generateRow(row)
+        result.push(row)
+
+        
+    }
+    return result
 
 }
 console.log(generate(5));
