21a330ba5eec532873cb7c1a8d9a76a88b9e9eab
Azimjon030201 <hazard030201@gmail.com>
2025-10-13 16:29:49 +0500

    0 larni orqaga surish qayta ishlandi

--- Arrays/Nollarni orqaga surish/index.js
+++ Arrays/Nollarni orqaga surish/index.js
@@ -1,20 +1,32 @@
 function moveZeroes(nums){
-    let start = 0
-    let end = nums.length - 1
+    // let start = 0
+    // let end = nums.length - 1
     
     
-    while(start<end){
-        if(nums[end]== 0){
-            end--
-        }
-        if(nums[start] == 0){
-            let change = nums[end]
-            nums[end] = nums[start]
-            nums[start] = change; 
-            end--
-        }
-        start++
+    // while(start<end){
+    //     if(nums[end]== 0){
+    //         end--
+    //     }
+    //     if(nums[start] == 0){
+    //         let change = nums[end]
+    //         nums[end] = nums[start]
+    //         nums[start] = change; 
+    //         end--
+    //     }
+    //     start++
 
+    // }
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] == 0){
+            count ++
+            continue;
+        }
+        let temp = nums[i];
+        nums[i] = nums[i - count]
+        nums[i - count] = temp
+        
+        
     }
     return nums
 }
---
+++ Arrays/Paskal uchburchagi/index.js
@@ -0,0 +1,12 @@
+function generate(n){
+    let result = [[1]]
+    let startingArr = [1,1]
+    for(let i = 1; i<n; i++){
+        
+        
+    }
+
+}
+console.log(generate(5));
+
+module.exports = {generate}
