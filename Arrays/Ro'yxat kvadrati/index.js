ebc5b003afe0eac8dd18ef9cca9050577718622b
Azimjon030201 <hazard030201@gmail.com>
2025-10-15 10:12:25 +0500

    Ro'yxat kvadrati qayta ishlandi

--- Arrays/Ro'yxat kvadrati/index.js
+++ Arrays/Ro'yxat kvadrati/index.js
@@ -1,5 +1,18 @@
 function sortedSquares(nums){
-   return nums.map((item) =>{return item*item}).sort((a,b) =>{return a - b})
+   // return nums.map((item) =>{return item*item}).sort((a,b) =>{return a - b})
+   let start = 0
+   let end = nums.length - 1
+   let result = []
+   while(start <= end){
+      if(Math.pow(nums[start],2)>= Math.pow(nums[end],2)){
+         result.unshift(Math.pow(nums[start],2))
+         start++
+      }else{
+         result.unshift(Math.pow(nums[end],2))
+         end--
+      }
+   }
+   return result
 }
 console.log(sortedSquares([-4,-1,0,3,10]));
 
